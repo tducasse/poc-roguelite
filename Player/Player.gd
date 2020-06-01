bc1a064f5781a98932ebd0314cf47dc522850bec
tducasse <thibaud.ducasse@gmail.com>
2020-06-01 19:11:51 +1000

    Add player idle animations

--- Player/Player.gd
+++ Player/Player.gd
@@ -10,15 +10,37 @@ const UI_LEFT = "ui_left"
 const UI_UP = "ui_up"
 const UI_DOWN = "ui_down"
 
+# Animation parameters
+const PLAYBACK_NAME = "parameters/playback"
+const IDLE_BLEND_POS = "parameters/Idle/blend_position"
+const IDLE_STATE = "Idle"
+
+# Local vars
 var velocity = Vector2.ZERO
 
+# onready
+onready var anim_tree := $AnimationTree
+onready var anim_state = anim_tree.get(PLAYBACK_NAME)
+
+
+func _ready():
+	anim_tree.active = true
+	# turn right by default
+	anim_tree.set(IDLE_BLEND_POS, Vector2(1, 0))
+	anim_state.travel(IDLE_STATE)
+
+
 func _physics_process(delta):
-	var inputVector = Vector2(
+	var input_vector = Vector2(
 		Input.get_action_strength(UI_RIGHT) - Input.get_action_strength(UI_LEFT),
 		Input.get_action_strength(UI_DOWN) - Input.get_action_strength(UI_UP)
 	).normalized()
 
-	velocity = velocity.move_toward(inputVector * MAX_SPEED, ACCELERATION * delta)
+	if input_vector != Vector2.ZERO:
+		anim_tree.set(IDLE_BLEND_POS, input_vector)
+	anim_state.travel(IDLE_STATE)
+
+	velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 	velocity = move_and_slide(velocity)
 	
 	
