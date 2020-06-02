b4db0214dc8cbcfb329496f725d00757d0fe796e
tducasse <thibaud.ducasse@gmail.com>
2020-06-02 15:03:16 +1000

    Fix drop_items not using call_deferred()

--- Slime/Slime.gd
+++ Slime/Slime.gd
@@ -20,7 +20,13 @@ func drop_items():
 func _on_Hurtbox_area_entered(area):
 	stats.health -= area.damage
 
-
-func _on_Stats_no_health():
+func drop_and_die():
 	drop_items()
 	queue_free()
+
+func _on_Stats_no_health():
+	# We call drop_items, which tries to add a child that contains an Area.
+	# Since the current function is called as as a result of processing the area
+	# collisions, adding an area is forbidden via a mutex (endless loop).
+	# Using call_deferred tells the engine to call the function once we're idle.
+	call_deferred("drop_and_die")
